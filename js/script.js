c7ccf6c83b42045cd732298221d1ed6e5226c1dd
lapivoinerouge <wioladys@gmail.com>
2022-08-15 19:27:44 +0200

    Add/remove active class on article link click

---
+++ js/script.js
@@ -0,0 +1,33 @@
+{
+    'use strict;'
+
+    const titleClickHandler = function(event){
+        const clickedElement = this;
+        /* remove class 'active' from all article links  */
+        const activeLinks = document.querySelectorAll('.titles a.active');
+        for (let activeLink of activeLinks) {
+            activeLink.classList.remove('active');
+        }
+      
+        /* add class 'active' to the clicked link */
+        clickedElement.classList.add('active');
+      
+        /* remove class 'active' from all articles */
+        const activeArticles = document.querySelectorAll('.posts article.active');
+        for (let activeArticle of activeArticles) {
+            activeArticle.classList.remove('active');
+        }
+      
+        /* get 'href' attribute from the clicked link */
+      
+        /* find the correct article using the selector (value of 'href' attribute) */
+      
+        /* add class 'active' to the correct article */
+      }
+      
+      const links = document.querySelectorAll('.titles a');
+      
+      for (let link of links){
+        link.addEventListener('click', titleClickHandler);
+      }
+}
